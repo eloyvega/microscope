5c6fff62917971a9148b81ac4916fa21200d30cb
Eloy Vega <eloy.vega@gmail.com>
2014-12-16 06:54:58 -0600

    Fix error on post_submit.js

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -1,7 +1,9 @@
-Template.postSubmit.events({ 'submit form': function(e) {
+Template.postSubmit.events({ 
+  'submit form': function(e) {
     e.preventDefault();
     var post = {
-    url:  t).find('[name=url]').val(), title: $(e.target).find('[name=title]').val()
+      url: $(e.target).find('[name=url]').val(), 
+      title: $(e.target).find('[name=title]').val()
     };
     post._id = Posts.insert(post);
     Router.go('postPage', post);
